4cb484b8d7c03dbd654806670aeb0e027ff7d880
johnthagen <johnthagen@gmail.com>
2021-02-03 09:26:05 -0500

    Create reproducible example showing export bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MaterialTable, { Column } from "@material-table/core";
+import { ExportCsv, ExportPdf } from "@material-table/exporters";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IPerson {
+  firstName: string;
+  lastName: string;
+  birthYear: number;
+  availability: boolean;
 }
 
-export default App;
+export default function App() {
+  const lookup = { true: "Available", false: "Unavailable" };
+
+  const columns: Array<Column<IPerson>> = [
+    { title: "First Name", field: "firstName" },
+    { title: "Last Name", field: "lastName" },
+    { title: "Birth Year", field: "birthYear", type: "numeric" },
+    { title: "Availablity", field: "availability", lookup },
+  ];
+
+  const data: Array<IPerson> = [
+    {
+      firstName: "Tod",
+      lastName: "Miles",
+      birthYear: 1987,
+      availability: true,
+    },
+    {
+      firstName: "Jess",
+      lastName: "Smith",
+      birthYear: 2000,
+      availability: false,
+    },
+  ];
+
+  return (
+    <MaterialTable
+      columns={columns}
+      data={data}
+      options={{
+        exportMenu: [
+          {
+            label: "Export PDF",
+            exportFunc: (cols, datas) =>
+              ExportPdf(cols, datas, "myPdfFileName"),
+          },
+          {
+            label: "Export CSV",
+            exportFunc: (cols, datas) =>
+              ExportCsv(cols, datas, "myCsvFileName"),
+          },
+        ],
+      }}
+    />
+  );
+}
